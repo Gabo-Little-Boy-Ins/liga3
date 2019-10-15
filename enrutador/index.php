0f774b85fbb1e9142176470f8270e598d551bd6a
Jonathan Gabirel Cervantes Ruiz <54849692+Gabo-Little-Boy-Ins@users.noreply.github.com>
2019-10-15 07:32:34 -0500

    Add files via upload

--- enrutador/index.php
+++ enrutador/index.php
@@ -1,4 +1,8 @@
 <?php
+function miFuncion($a, $b){
+ return $a+$b;
+}
+
  // Cargamos LIGA3
  require_once '../LIGA3/LIGA.php';
 
@@ -6,6 +10,58 @@
  RUTA::nueva('holaMundo', function() {
   echo '<h1>Hola mundo con el enrutador de LIGA.php</h1>';
  });
+ RUTA::nueva('Yadira/tarea', function() {
+  echo "<p>tarea dia 10/10/2019 clase SCyD.</p>";
+ });
+
+   //funcion anonima
+ RUTA::nueva('Bienvenida ', function() {
+  echo "<p>Bienvenid@ Usuari@</p>";
+ });
+ 
+ RUTA::nueva('usuario/insertar', function() {
+ $liga = LIGA('base.usuarios');
+ $resp = $liga ->insertar($_POST);
+ if ($resp ==1){
+  echo 'USUARIO INSERTADO EXITOSAMENTE';
+ }else{
+  echo"Error: $resp";
+ }
+ });
+ 
+RUTA::nueva('usuario/forma/modificar', function() {
+		$liga = LIGA('base.usuarios');
+		$prop = array('select'=>'id="cual" name="cual"',
+																'option'=>'value="@[0]"'
+																);
+		$selector = HTML::selector($liga, '1', $prop);
+		$campos = array('cual'=> $selector, '*');
+		$props = array('form'=>'action="../modificar" method="POST"');
+		HTML::forma($liga, 'Insertar usuarios', $campos, $props);
+	});
+  
+  
+  
+  
+   RUTA::nueva('usuario/tabla', function() {
+  $liga = LIGA('base.usuarios');
+    HTML::tabla($liga, 'insertar usuarios');
+ });
+ 
+  	RUTA::nueva('usuario/modificar', function() {
+		$liga = LIGA('base.usuarios');
+		//var_dump($_POST);
+		$datos = array($_POST['cual']=>$_POST);
+		$resp = $liga->modificar($datos);
+		if ($resp == 1) {
+			echo 'Usuario modificado exitosamente';
+		} else {
+			echo "Error: $resp";
+		}
+		RUTA::run('usuario/tabla');
+	});
+   
+ 
   // Imprimo las etiquetas HTML iniciales
   HTML::cabeceras(array('title'      =>'RUTA en LIGA 3',
 			'description'=>'Página de pruebas para RUTA de LIGA 3',
